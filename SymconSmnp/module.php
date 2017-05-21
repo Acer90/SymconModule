5de18aa691951b5ea2d7f8561b3ac48034f5e250
Acer90 <acer151090@aol.com>
2017-05-21 20:26:12 +0200

    change module.php

--- SymconSmnp/module.php
+++ SymconSmnp/module.php
@@ -4,12 +4,12 @@
  
         // Der Konstruktor des Moduls
         // Überschreibt den Standard Kontruktor von IPS
-        public function __construct($InstanceID) {
+        //public function __construct($InstanceID) {
             // Diese Zeile nicht löschen
-            parent::__construct($InstanceID);
+            //parent::__construct($InstanceID);
 
             // Selbsterstellter Code
-        }
+        //}
 
         // Überschreibt die interne IPS_Create($id) Funktion
         public function Create() {
