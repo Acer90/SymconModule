d506c7f81eaa3cdf53ead67ebdb804b4713aa9e4
Acer90 <acer151090@aol.com>
2017-05-21 20:28:07 +0200

    test

--- SymconSmnp/module.php
+++ SymconSmnp/module.php
@@ -1,28 +1,6 @@
 <?
     // Klassendefinition
     class IPSWINSNMP extends IPSModule {
- 
-        // Der Konstruktor des Moduls
-        // Überschreibt den Standard Kontruktor von IPS
-        //public function __construct($InstanceID) {
-            // Diese Zeile nicht löschen
-            //parent::__construct($InstanceID);
-
-            // Selbsterstellter Code
-        //}
-
-        // Überschreibt die interne IPS_Create($id) Funktion
-        public function Create() {
-            // Diese Zeile nicht löschen.
-            parent::Create();
-
-        }
-
-        // Überschreibt die intere IPS_ApplyChanges($id) Funktion
-        public function ApplyChanges() {
-            // Diese Zeile nicht löschen
-            parent::ApplyChanges();
-        }
 
         /**
         * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
