923e3eac2134849b7ae6fba2f29668feec9bcc53
Acer90 <acer151090@aol.com>
2017-05-23 14:21:31 +0200

    change modul

--- SymconSmnp/module.php
+++ SymconSmnp/module.php
@@ -4,7 +4,7 @@
         public function __construct($InstanceID) {
             parent::__construct($InstanceID);
         }
-        
+
         public function Create() {
             parent::Create();
 
@@ -13,12 +13,23 @@
             $this->RegisterPropertyInteger("SNMPPort", 161);
             $this->RegisterPropertyInteger("SNMPTimeout", 1);
             $this->RegisterPropertyInteger("SNMPVersion", 2);
+
             $this->RegisterPropertyString("SNMPCommunity", "public"); 
+
+            $this->RegisterPropertyString("SNMPSecurityName", "");
+            $this->RegisterPropertyString("SNMPAuthenticationProtocol", "SHA"); 
+            $this->RegisterPropertyString("SNMPAuthenticationPassword", ""); 
+            $this->RegisterPropertyString("SNMPPrivacyProtocol", "AES256"); 
+            $this->RegisterPropertyString("SNMPPrivacyPassword", ""); 
+            $this->RegisterPropertyString("SNMPEngineID", ""); 
+            $this->RegisterPropertyString("SNMPContextName", ""); 
+            $this->RegisterPropertyString("SNMPContextEngine", ""); 
         }
 
         public function ApplyChanges() {
             // Diese Zeile nicht löschen
             parent::ApplyChanges();
+            $this->RequireParent("{1A75660D-48AE-4B89-B351-957CAEBEF22D}");
         }
 
         public function MeineErsteEigeneFunktion() {
