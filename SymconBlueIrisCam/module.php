27e0e85b957508a464b42d0d8afd06c9236292ea
Acer90 <acer151090@aol.com>
2017-06-24 13:21:28 +0200

    1

--- SymconBlueIrisCam/module.php
+++ SymconBlueIrisCam/module.php
@@ -1,6 +1,6 @@
 <?
     // Klassendefinition
-    class BlueIris extends IPSModule {
+    class BlueIrisCam extends IPSModule {
         public function __construct($InstanceID) {
             parent::__construct($InstanceID);
         }
@@ -9,23 +9,8 @@
             parent::Create();
 
             // Modul-Eigenschaftserstellung
-            $this->RegisterPropertyString("IPAddress", "192.168.178.1"); 
-            $this->RegisterPropertyInteger("Port", 81);
-            $this->RegisterPropertyInteger("Timeout", 3);
-            $this->RegisterPropertyInteger("Interval", 10);
-            $this->RegisterPropertyString("Username", "admin");
-            $this->RegisterPropertyString("Password", "");
+            $this->RegisterPropertyString("ShortName", "cam1");
 
-            //event erstellen
-            $this->RegisterTimer("SyncData", $this->ReadPropertyInteger("Interval"), 'IPSWINSNMP_SyncData($_IPS[\'TARGET\']);');
-        }
-
-        public function ApplyChanges() {
-            // Diese Zeile nicht löschen
-            parent::ApplyChanges();
-            //$this->RequireParent("{1A75660D-48AE-4B89-B351-957CAEBEF22D}");
-
-            $this->SetTimerInterval("SyncData", $this->ReadPropertyInteger("Interval")*1000);
         }
 
         public function Login(){
@@ -435,7 +420,7 @@
 
             $output = json_decode($result, true);
             if($output["result"] == "success"){ 
-                return $output;
+                return $output["data"];
             }else{
                 return [];
             };
