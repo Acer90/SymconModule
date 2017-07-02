77b7530bffcfbc68bb0a65343b5b60be621f0146
Acer90 <acer151090@aol.com>
2017-07-02 17:17:44 +0200

    1

--- SymconSamsungTizen/module.php
+++ SymconSamsungTizen/module.php
@@ -67,8 +67,9 @@
             $sp = websocket_open($broadcast,8001, "/api/v2/channels/samsung.remote.control", $headers,$errstr,$timeout);
             if($sp){
                 if(is_null($key)) return true;
-                echo $data = websocket_read($sp,$errstr);
+                
                 $bytes_written = websocket_write($sp,$send_data);
+                echo $data = websocket_read($sp,$errstr);
                 if($bytes_written){
                     echo $data = websocket_read($sp,$errstr);
                     echo "Server responed with: " . $errstr ? $errstr : $data;
