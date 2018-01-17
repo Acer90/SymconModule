9ba8c21b4c136c2e81a724eb12a190b2987a48dd
Acer90 <acer151090@aol.com>
2018-01-17 18:00:06 +0100

    New WoL Funktion

--- SymconSamsungTizen/module.php
+++ SymconSamsungTizen/module.php
@@ -1,5 +1,5 @@
 <?
-    // Klassendefinition
+ // Klassendefinition
     class SamsungTizen extends IPSModule {
         public function __construct($InstanceID) {
             parent::__construct($InstanceID);
@@ -17,8 +17,10 @@
             $this->RegisterPropertyInteger("VariableOnline", 0);
             $this->RegisterPropertyInteger("InstanceWebSocket", 0); 
 
+            $this->ConnectParent("{3AB77A94-3467-4E66-8A73-840B4AD89582}");  
+            $this->GetConfigurationForParent();
 
-            //event erstellen
+            //event erstellen 
             $this->RegisterTimer("CheckOnline", $this->ReadPropertyInteger("Interval"), 'SamsungTizen_CheckOnline($_IPS[\'TARGET\']);');
             $this->SetStatus(102);
         }
@@ -29,24 +31,47 @@
 
             $this->SetStatus(102);
             $this->SetTimerInterval("CheckOnline", $this->ReadPropertyInteger("Interval")*1000);
+
+            $this->ConnectParent("{3AB77A94-3467-4E66-8A73-840B4AD89582}"); 
+            $this->GetConfigurationForParent();
         }
 
         public function WakeUp(){
             $broadcast = $this->ReadPropertyString("IPAddress");
             $mac_addr = $this->ReadPropertyString("MACAddress");
 
-            if (!$fp = fsockopen('udp://' . $broadcast, 2304, $errno, $errstr, 10)) 
-                return false; 
-
-            $mac_hex = preg_replace('=[^a-f0-9]=i', '', $mac_addr); 
-
-            $mac_bin = pack('H12', $mac_hex); 
-
-            $data = str_repeat("\xFF", 6) . str_repeat($mac_bin, 16); 
-
-            fputs($fp, $data); 
-            fclose($fp); 
-            return true; 
+            $addr_byte = explode(':', $mac_addr);  
+            $hw_addr = '';  
+            
+            for ($a=0; $a < 6; $a++) $hw_addr .= chr(hexdec($addr_byte[$a]));  
+            
+            $msg = chr(255).chr(255).chr(255).chr(255).chr(255).chr(255);  
+            
+            for ($a = 1; $a <= 16; $a++) $msg .= $hw_addr;  
+            
+            // send it to the broadcast address using UDP  
+            // SQL_BROADCAST option isn't help!!  
+            $s = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);  
+            if ($s == false)  
+            {  
+                echo "Error creating socket!\n";  
+                echo "Error code is '".socket_last_error($s)."' - " . socket_strerror(socket_last_error($s));  
+                return false;
+            }  
+            else  
+            {  
+                // setting a broadcast option to socket:  
+                $opt_ret = socket_set_option($s, 1, 6, TRUE);  
+                if($opt_ret < 0)  
+                {  
+                    echo "setsockopt() failed, error: " . strerror($opt_ret) . "\n";  
+                }  
+                $e = socket_sendto($s, $msg, strlen($msg), 0, $broadcast, 2050);  
+                echo $e; 
+                socket_close($s);  
+                echo "Magic Packet sent (".$e.") to ".$broadcast.", MAC=".$mac_addr; 
+                return true; 
+            } 
 
         }
 
@@ -69,8 +94,14 @@
         public function CheckOnline(){
             $Intid = $this->InstanceID;
             $varonline = $this->ReadPropertyInteger("VariableOnline");
+            $int_websocket = $this->ReadPropertyInteger("InstanceWebSocket"); 
+
             if(IPS_VariableExists($varonline) && IPS_GetVariable($varonline)["VariableType"] == 0){
-                $resultat = @$this->SendDataToParent(json_encode(Array("DataID" => "{BC49DE11-24CA-484D-85AE-9B6F24D89321}", "FrameTyp" => 1, "Fin" => true, "Buffer" => ""))); 
+                //old Fucntion
+                //$resultat = @$this->SendDataToParent(json_encode(Array("DataID" => "{BC49DE11-24CA-484D-85AE-9B6F24D89321}", "FrameTyp" => 1, "Fin" => true, "Buffer" => ""))); 
+                $send_str = '{"method":"ms.remote.control","params":{"Cmd":"Click","DataOfCmd":"","Option":"false","TypeOfRemote":"SendRemoteKey"}}';
+                $resultat = WSC_SendPing($int_websocket, $send_str);
+                
                 //IPS_LogMessage("ReceiveData", $resultat);    
                 if($resultat == 1 || $resultat == true){
                     $resultat = true;
