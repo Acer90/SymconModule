a3e15b365535e97a873e4f976f937209430ca07c
Acer90 <acer151090@aol.com>
2018-01-18 17:44:38 +0100

    test

--- SymconSamsungTizen/module.php
+++ SymconSamsungTizen/module.php
@@ -4,42 +4,42 @@
         public function __construct($InstanceID) {
             parent::__construct($InstanceID);
         }
-　
+
         public function Create() {
             parent::Create();
-　
+
             // Modul-Eigenschaftserstellung
             $this->RegisterPropertyString("IPAddress", "192.168.178.1"); 
             $this->RegisterPropertyString("MACAddress", "aa:bb:cc:00:11:22"); 
             $this->RegisterPropertyInteger("Interval", 10);
             $this->RegisterPropertyInteger("Sleep", 1000);
-　
+
             $this->RegisterPropertyInteger("VariableOnline", 0);
             $this->RegisterPropertyInteger("InstanceWebSocket", 0); 
-　
+
             $this->ConnectParent("{3AB77A94-3467-4E66-8A73-840B4AD89582}");  
             $this->GetConfigurationForParent();
-　
+
             //event erstellen 
             $this->RegisterTimer("CheckOnline", $this->ReadPropertyInteger("Interval"), 'SamsungTizen_CheckOnline($_IPS[\'TARGET\']);');
             $this->SetStatus(102);
         }
-　
+        　
         public function ApplyChanges() {
             // Diese Zeile nicht löschen
             parent::ApplyChanges();
-　
+            　
             $this->SetStatus(102);
             $this->SetTimerInterval("CheckOnline", $this->ReadPropertyInteger("Interval")*1000);
-　
+            　
             $this->ConnectParent("{3AB77A94-3467-4E66-8A73-840B4AD89582}"); 
             $this->GetConfigurationForParent();
         }
-　
+        　
         public function WakeUp(){
             $broadcast = $this->ReadPropertyString("IPAddress");
             $mac_addr = $this->ReadPropertyString("MACAddress");
-　
+            　
             $addr_byte = explode(':', $mac_addr);  
             $hw_addr = '';  
             
@@ -72,27 +72,22 @@
                 //echo "Magic Packet sent (".$e.") to ".$broadcast.", MAC=".$mac_addr; 
                 return true; 
             } 
-　
         }
-　
-	public function WakeUp_Win(){
+	    public function WakeUp_Win(){
             $broadcast = $this->ReadPropertyString("IPAddress");
             $mac_addr = $this->ReadPropertyString("MACAddress");
-　
             if (!$fp = fsockopen('udp://' . $broadcast, 2304, $errno, $errstr, 10)) 
                 return false; 
-　
+
             $mac_hex = preg_replace('=[^a-f0-9]=i', '', $mac_addr); 
-　
             $mac_bin = pack('H12', $mac_hex); 
-　
             $data = str_repeat("\xFF", 6) . str_repeat($mac_bin, 16); 
-　
+
             fputs($fp, $data); 
             fclose($fp); 
             return true; 
         }
-　
+
         public function SendKeys(String $keys){
             $sleep = $this->ReadPropertyInteger("Sleep");
             $sleep = $sleep / 1000;
@@ -108,12 +103,12 @@
                 $resultat = $this->SendDataToParent(json_encode(Array("DataID" => "{BC49DE11-24CA-484D-85AE-9B6F24D89321}", "FrameTyp" => 1, "Fin" => true, "Buffer" => $send_str))); 
             }
         }
-　
+
         public function CheckOnline(){
             $Intid = $this->InstanceID;
             $varonline = $this->ReadPropertyInteger("VariableOnline");
             $int_websocket = $this->ReadPropertyInteger("InstanceWebSocket"); 
-　
+
             if(IPS_VariableExists($varonline) && IPS_GetVariable($varonline)["VariableType"] == 0){
                 //old Fucntion
                 //$resultat = @$this->SendDataToParent(json_encode(Array("DataID" => "{BC49DE11-24CA-484D-85AE-9B6F24D89321}", "FrameTyp" => 1, "Fin" => true, "Buffer" => ""))); 
@@ -129,15 +124,15 @@
                 SetValueBoolean($varonline, $resultat);
             }
         }
-　
+
         public function ReceiveData($JSONString) {
                $data = json_decode($JSONString);
                //IPS_LogMessage("ReceiveData", utf8_decode($data->Buffer));          
         }
-　
+
         public function GetConfigurationForParent() {
             $ipAdress = $this->ReadPropertyString("IPAddress");
-　
+
             $TizenAdress = "ws://".$ipAdress.":8001/api/v2/channels/samsung.remote.control";
             return "{\"URL\": \"".$TizenAdress."\"}";
         }
