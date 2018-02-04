e354034f8939abe5f5f32edbe16c3a156eebcc21
Acer90 <acer151090@aol.com>
2018-02-04 17:31:01 +0100

    Add check Speed button

--- SymconSmnp/module.php
+++ SymconSmnp/module.php
@@ -1,7 +1,6 @@
 <?
     require('lib/snmp.php');
-    
-    // Klassendefinition
+
     class IPSSNMP extends IPSModule {
         public function __construct($InstanceID) {
             parent::__construct($InstanceID);
