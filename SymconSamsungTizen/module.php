7e6c1ad5432acb8a6d1434ba2bd2746de45fb122
Acer90 <acer151090@aol.com>
2019-03-23 14:29:08 +0100

    SymconSamsungTizen Fix: ClientName

--- SymconSamsungTizen/module.php
+++ SymconSamsungTizen/module.php
@@ -185,13 +185,13 @@
 
                 $token = $this->GetValue("VariableToken");
                 if(empty($token)){
-                    $address = "wss://".$ipAdress.":8002/api/v2/channels/samsung.remote.control?name=".base64_encode("symcon");
+                    $address = "wss://".$ipAdress.":8002/api/v2/channels/samsung.remote.control?name=SVBTeW1jb25UaXplbg==";
                 }else{
-                    $address = "wss://".$ipAdress.":8002/api/v2/channels/samsung.remote.control?name=".base64_encode("symcon")."&token=".$token;
+                    $address = "wss://".$ipAdress.":8002/api/v2/channels/samsung.remote.control?name=SVBTeW1jb25UaXplbg==&token=".$token;
                 }
             }else{
                 $origin = "http://".$ipAdress.":8001";
-                $address = "ws://".$ipAdress.":8001/api/v2/channels/samsung.remote.control?name=".base64_encode("symcon");
+                $address = "ws://".$ipAdress.":8001/api/v2/channels/samsung.remote.control?name=SVBTeW1jb25UaXplbg==";
             }
 
             //"Open": ".$active.",
