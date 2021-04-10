debca4d4a2f5d71bcbc97429ecb61fdd5abe990e
acer90 <acer151090@aol.com>
2021-04-10 13:47:49 +0200

    Add: SymconJSLive SymconJSLiveChart SymconJSLiveDoughnut-Pie SymconJSLiveGauge SymconJSLiveRadarChart SymconJSLiveColorPicker

--- SymconJSLiveColorPicker/module.php
+++ SymconJSLiveColorPicker/module.php
@@ -50,7 +50,7 @@ class SymconJSLiveColorPicker extends JSLiveModule{
             case "getData":
                 return $this->GetData($buffer['queryData']);
             case "setData":
-                return ""; //$this->GetData($buffer['queryData']);
+                return $this->SetData($buffer['queryData']);
             default:
                 $this->SendDebug("ReceiveData", "ACTION " . $buffer['cmd'] . " FOR THIS MODULE NOT DEFINED!", 0);
                 break;
@@ -82,6 +82,33 @@ class SymconJSLiveColorPicker extends JSLiveModule{
     }
     public function GetData($querydata){
         return json_encode($this->GenerateVariabels());
+    }
+
+    public function SetData($querydata){
+        if(!array_key_exists("var", $querydata) || !array_key_exists("val", $querydata)){
+            $this->SendDebug('SetData', "NO VARIABLE, OR VALUE SET!", 0);
+            return "NO VARIABLE, OR VALUE SET!";
+        }
+
+        $var_data = json_decode($this->ReadPropertyString("Variables"),true);
+        $var_ids = array();
+        foreach ($var_data as $varItem){
+            if($varItem["Color"] > 0)
+                $var_ids[] = $varItem["Color"];
+
+            if($varItem["ColorTemperature"] > 0)
+                $var_ids[] = $varItem["ColorTemperature"];
+        }
+
+        if(!in_array($querydata["var"], $var_ids)){
+            $this->SendDebug('SetData', "VARIABLE NOT IN LIST SET!", 0);
+            return "VARIABLE NOT IN LIST SET!";
+        }
+
+        $this->SendDebug("SetData", "Update Variable " . $querydata["var"] ." => " .$querydata["val"], 0 );
+        RequestAction($querydata["var"], $querydata["val"]);
+        $this->SendDebug("SetData", "Update Variable => OK", 0 );
+        return "OK";
     }
 
     private function ReplacePlaceholder($htmlData){
