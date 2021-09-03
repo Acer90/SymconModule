73c34bbff31c7e120b442520c9874504210379e6
Acer90 <acer151090@aol.com>
2021-09-03 19:03:58 +0200

    SNMP Change to new lib

--- SymconSmnp/vendor/composer/installed.php
+++ SymconSmnp/vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
         'type' => 'library',
-        'install_path' => __DIR__ . '/../snmplib/',
+        'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => 'e0139601e1894733002db168491e8d61e598ad03',
         'name' => '__root__',
         'dev' => true,
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
             'type' => 'library',
-            'install_path' => __DIR__ . '/../snmplib/',
+            'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => 'e0139601e1894733002db168491e8d61e598ad03',
             'dev_requirement' => false,
         ),
         'freedsx/asn1' => array(
